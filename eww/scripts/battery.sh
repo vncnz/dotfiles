7f544c74cb9c99c8008b10c6318dc1bdffaa9e66
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2024-07-15 23:22:05 +0200

    Batteria e network nuovi, altre modifiche minori

---
+++ eww/scripts/battery.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+percentage () {
+  local val=$(echo $1 | tr '%' ' ' | awk '{print $1}')
+  local icon1=$2
+  local icon2=$3
+  local icon3=$4
+  local icon4=$5
+  local icon5=$6
+  if [ "$val" -le 15 ]; then
+    echo $icon1
+  elif [ "$val" -le 35 ]; then
+    echo $icon2
+  elif [ "$val" -le 65 ]; then
+    echo $icon3
+  elif [ "$val" -le 85 ]; then
+    echo $icon4
+  else
+    echo $icon5
+  fi
+}
+
+get_icon () {
+    # local br=$(get_percent)
+    if [ "$state" == "fully-charged" ]; then
+        echo ""
+    elif [ "$state" == "charging" ]; then
+        echo ""
+    elif [ "$state" == "pending-charge" ]; then
+        echo ""
+    elif [ "$state" == "unknown" ]; then
+        echo ""
+    else
+        echo $(percentage "$PERCENTAGE" "" "" "" "" "")
+    fi
+}
+
+DEVICE_PATH="/org/freedesktop/UPower/devices/battery_BAT0"
+INFORMATION=$(upower -i "$DEVICE_PATH" 2> /dev/null)
+PERCENTAGE=$(awk '/percentage/ {print $NF}' <<< $INFORMATION)
+CAPACITY=$(awk '/capacity/ {print $NF}' <<< $INFORMATION)
+UPDATED=$(awk -F '(' '/updated/ {print $NF}' <<< $INFORMATION | sed -r 's/\)//')
+TIME_TO_EMPTY=$(awk -F ':' '/time to empty/ {print $NF}' <<< $INFORMATION | sed -r 's/\s{2,}//g')
+state=$(awk '/state/ {print $NF}' <<< $INFORMATION)
+
+icon=$(get_icon)
+
+PERCENTAGE=$(echo $PERCENTAGE | tr '%' ' ' | awk '{print $1}')
+CAPACITY=$(echo $CAPACITY | tr '%' ' ' | awk '{print $1}')
+
+echo '{"percentage": "'"$PERCENTAGE"'", "capacity": "'"$CAPACITY"'", "updated": "'"$UPDATED"'", "eta": "'"$TIME_TO_EMPTY"'", "icon": "'"$icon"'", "state": "'"$state"'"}'
---
+++ eww/scripts/network.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+percentage () {
+  local val=$(echo $1 | tr '%' ' ' | awk '{print $1}')
+  local icon1=$2
+  local icon2=$3
+  local icon3=$4
+  local icon4=$5
+  if [ "$val" -le 15 ]; then
+    echo $icon1
+  elif [ "$val" -le 30 ]; then
+    echo $icon2
+  elif [ "$val" -le 60 ]; then
+    echo $icon3
+  else
+    echo $icon4
+  fi
+}
+
+get_icon () {
+    # local br=$(get_percent)
+    if [ "$essid" == "lo" ]; then
+        echo ""
+    elif [ "$wired" == "1" ]; then
+        echo ""
+    else
+        echo $(percentage "$signal" "" "" "" "")
+    fi
+}
+
+signal=$(nmcli -f in-use,signal dev wifi | rg "\*" | awk '{ print $2 }')
+essid=$(nmcli -t -f NAME connection show --active | head -n1 | sed 's/\"/\\"/g')
+wired=$(nmcli device status | grep connected | grep -c Wired)
+icon=$(get_icon)
+echo '{"essid": "'"$essid"'", "signal": "'"$signal"'", "icon": "'"$icon"'", "wired": "'"$wired"'"}'
+
+ip monitor link | while read -r line; do
+    signal=$(nmcli -f in-use,signal dev wifi | rg "\*" | awk '{ print $2 }')
+    essid=$(nmcli -t -f NAME connection show --active | head -n1 | sed 's/\"/\\"/g')
+    wired=$(nmcli device status | grep connected | grep -c Wired)
+    icon=$(get_icon)
+    echo '{"essid": "'"$essid"'", "signal": "'"$signal"'", "icon": "'"$icon"'", "wired": "'"$wired"'"}'
+done
