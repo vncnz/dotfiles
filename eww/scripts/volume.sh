b5f0a7e4bcefb6cc444c4e0af071fa9721a48795
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2024-07-23 14:25:32 +0200

    Volume indicator experiments

--- eww/scripts/volume.sh
+++ eww/scripts/volume.sh
@@ -89,4 +89,17 @@ if [[ $1 == "set" ]]; then
     val=100
   fi
   pactl set-sink-volume $SINK $val%
+  # wpctl set-volume 
+  # $(~/.config/eww/scripts/dunstvolume.sh)
+  muted=$(is_muted)
+  volume=$(get_vol)
+  volume100=$(awk '{ print $1 / 100 }' <<< $volume)
+  
+  msgId="3378423"
+  if [[ $volume == 0 || "$muted" == "1" ]]; then
+    dunstify -a "changeVolume" -u low -i audio-volume-muted -r "$msgId" "Volume muted"
+  else
+    dunstify -a "changeVolume" -u low -i audio-volume-high -r "$msgId" \
+    -h int:value:"$volume100" "Volume: ${volume}%"
+  fi
 fi
