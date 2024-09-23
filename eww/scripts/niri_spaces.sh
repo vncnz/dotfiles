6954de29b02cac6679d64624e4b9fd9ccfa57dfe
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2024-09-23 16:43:58 +0200

    Niri windows experiments

---
+++ eww/scripts/niri_spaces.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ws=$(niri msg -j workspaces | jq -c -r '.[]')
+# for item in $ws; do
+#     echo $item
+# done
+
+
+# Usa il tuo comando che restituisce il JSON delle applicazioni aperte
+# json_output=$(niri msg -j windows)
+
+# Usa jq per estrarre i dati e raggruppare le applicazioni per workspace
+# applications=$(echo "$json_output" | jq -r '
+#     group_by(.workspace_id) | 
+#     map("Workspace \(.[]?.workspace_id):\n" + (unique | map("  - \(.title)") | join("\n"))) | 
+#     join("\n\n")
+# ')
+
+applications=$("/home/vncnz/.config/eww/scripts/niri_spaces.py")
+
+# Mostra il risultato con fuzzel
+c=$(echo "$applications" | fuzzel --dmenu --prompt="" --width 60)
+echo $c
