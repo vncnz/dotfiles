8c412230f3b8dd32a852b8582d54708777249058
vncnz <vincenzo.minolfi@gmail.com>
2025-10-09 12:39:50 +0200

    Moved and zipped obsolete configurations

--- waybar/modules/vpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-echo "Toggle switch VPN: $1 interface..."
-switcher=$( [ -d /proc/sys/net/ipv4/conf/"$1" ] && echo "stop" || echo "start";)
-sudo systemctl "$switcher" wg-quick@"$1"
-case ${switcher} in
-    "start") notify-send ' VPN is ON';;
-     "stop") notify-send ' VPN is OFF';;
-esac
