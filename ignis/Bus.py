3172691fe5c647e80c692e015a131ce1d98a869c
vncnz <vincenzo.minolfi@gmail.com>
2025-10-28 14:12:25 +0100

    Added topic support to bus

--- ignis/Bus.py
+++ ignis/Bus.py
@@ -1,11 +1,16 @@
 class Bus:
-    subscribers = []
+    subscribers = {}
 
     @classmethod
-    def subscribe(cls, fn):
-        cls.subscribers.append(fn)
+    def subscribe(cls, fn, topic='all'):
+        if not topic in cls.subscribers:
+            cls.subscribers[topic] = []
+        cls.subscribers[topic].append(fn)
+        print('subscribing', topic, cls.subscribers)
 
     @classmethod
-    def publish(cls, msg):
-        for fn in cls.subscribers:
-            fn(msg)
+    def publish(cls, msg, topic = 'all'):
+        if topic in cls.subscribers:
+            print('calling', topic, cls.subscribers)
+            for fn in cls.subscribers[topic]:
+                fn(msg)
