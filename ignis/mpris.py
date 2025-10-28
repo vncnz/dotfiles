c98a57e4570407ed2869f65a4cde69488608a04d
vncnz <vincenzo.minolfi@gmail.com>
2025-10-28 18:12:43 +0100

    Mpris

--- ignis/mpris.py
+++ ignis/mpris.py
@@ -3,3 +3,7 @@ from ignis.services.mpris import MprisService
 mpris = MprisService.get_default()
 
 mpris.connect("player_added", lambda x, player: print(player.desktop_entry, player.title))
+
+print(mpris.players)
+
+# 󰋙 󰫃 󰫄 󰫅 󰫆 󰫇 󰫈 󰋘
