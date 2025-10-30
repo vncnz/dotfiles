58ae9c5f3cfb663b8526a35b342889c2a9ab96f5
vncnz <vincenzo.minolfi@gmail.com>
2025-10-30 14:31:02 +0100

    BackgroundInfos moved to external file

---
+++ ignis/BackgroundInfos.py
@@ -0,0 +1,87 @@
+from ignis.widgets import Widget
+
+from ResBox import ResBox, WeatherBox, BatteryBox, MemoryBox, NetworkBox, RowBox
+from Bus import Bus
+from theme_colors import col, gra
+
+class BackgroundInfos (Widget.Window):
+    def __init__(self, monitor = None):
+
+        weather_box = WeatherBox()
+        battery_box = BatteryBox(None)
+        memory_used_box = MemoryBox()
+        network_box = NetworkBox()
+        multiline = RowBox()
+
+        self.weather_box = weather_box
+        self.battery_box = battery_box
+        self.memory_used_box = memory_used_box
+        self.network_box = network_box
+        self.multiline = multiline
+
+        # fetch = FetchService.get_default()
+        # ram = fetch.mem_info
+        # ram_used_perc = round(fetch.mem_used / fetch.mem_total * 100)
+
+        avg_load_label = ResBox('Load {value}', '')
+
+        # {'sensor': 'Tctl', 'value': 72.125, 'color': '#55FF00', 'icon': '\uf2cb', 'warn': 0.0}
+        temp_label = ResBox('Temperature {value}°', 0)
+
+        self.avg_load_label = avg_load_label
+        self.temp_label = temp_label
+
+        #battery_label = Widget.Label(
+        #     label="Hello world!"
+        # )
+
+        # p = UPowerService()
+        # battery_label.set_label(', '.join([f'Battery {batt.percent}%' for batt in p.batteries]) or 'No batteries')
+
+        # Utils.Poll(1000, self.update_ratatoskr)
+        
+        box = Widget.Box(
+            spacing = 6,
+            vertical = True,
+            child = [
+                weather_box,
+                Widget.Separator(vertical=False),
+                memory_used_box,
+                Widget.Separator(vertical=False),
+                avg_load_label,
+                Widget.Separator(vertical=False),
+                network_box,
+                Widget.Separator(vertical=False),
+                battery_box,
+                Widget.Separator(vertical=False),
+                multiline
+            ]
+        )
+
+        super().__init__(
+            namespace = 'background-infos',
+            monitor = monitor,
+            child = box,
+            layer = 'bottom',
+            anchor = ['bottom', 'left'],
+            margin_left = 70,
+            margin_bottom = 40
+        )
+        self.update_theme()
+        Bus.subscribe(lambda x: self.update_ratatoskr(x), topic='ratatoskr')
+    
+    def update_ratatoskr (self, rat):
+        if rat:
+            if 'weather' in rat: self.weather_box.update_value(rat['weather'])
+            if 'battery' in rat:
+                b = rat["battery"]
+                self.battery_box.update_value(b)
+            if 'ram' in rat: self.memory_used_box.update_value(rat['ram'])
+            if 'network'in rat: self.network_box.update_value(rat['network'])
+            if 'loadavg' in rat: self.avg_load_label.update_value(f'{rat['loadavg']['m1']} {rat['loadavg']['m5']} {rat['loadavg']['m15']}', color=gra(rat['loadavg']['warn']))
+            if 'temperature' in rat and 'disk' in rat and 'volume' in rat: self.multiline.update_value(rat['temperature'], rat['disk'], rat['volume'])
+
+            if False: battery_eta = (1, 273, .95)
+
+    def update_theme (self):
+        self.set_style(f'background-color:rgba(0,0,0,0.00001);color:{col('on_background')};')
--- ignis/config.py
+++ ignis/config.py
@@ -1,4 +1,3 @@
-from ignis.widgets import Widget
 from ignis.utils import Utils
 import json
 # import time
@@ -8,6 +7,7 @@ import os
 from gi.repository import GLib
 
 from ignis.app import IgnisApp
+
 app = IgnisApp.get_default()
 battery_eta = (0, 1, .5)
 
@@ -15,6 +15,7 @@ from Settings import config, reload_config
 
 from BackgroundNotif import BackgroundNotif
 from BackgroundSentence import BackgroundSentence
+from BackgroundInfos import BackgroundInfos
 from ForegroundInfos import ForegroundInfos
 from Frame import Frame
 from Bus import Bus
@@ -101,7 +102,7 @@ class CmdManager:
                     toggle_mode()
                     generate_theme(wallpaper)
                     if back: back.update_theme()
-                    fore.update_style()
+                    if fore: fore.update_style()
             else:
                 # EOF: riapri la fifo
                 return False
@@ -151,98 +152,6 @@ rat = read_ratatoskr_output()
 # from ignis.services.fetch import FetchService
 # from ignis.services.upower import UPowerService
 
-from ResBox import ResBox, WeatherBox, BatteryBox, MemoryBox, NetworkBox, RowBox
-
-class BackgroundInfos (Widget.Window):
-    def __init__(self, monitor = None):
-
-        weather_box = WeatherBox()
-        battery_box = BatteryBox(rat['battery'])
-        memory_used_box = MemoryBox()
-        network_box = NetworkBox()
-        multiline = RowBox()
-
-        self.weather_box = weather_box
-        self.battery_box = battery_box
-        self.memory_used_box = memory_used_box
-        self.network_box = network_box
-        self.multiline = multiline
-
-        # fetch = FetchService.get_default()
-        # ram = fetch.mem_info
-        # ram_used_perc = round(fetch.mem_used / fetch.mem_total * 100)
-
-        avg_load_label = ResBox('Load {value}', f'{rat['loadavg']['m1']} {rat['loadavg']['m5']} {rat['loadavg']['m15']}', color=gra(rat['loadavg']['warn']))
-
-        # {'sensor': 'Tctl', 'value': 72.125, 'color': '#55FF00', 'icon': '\uf2cb', 'warn': 0.0}
-        temp_label = ResBox('Temperature {value}°', rat['temperature']['value'], color=gra(rat['temperature']['warn']))
-
-        # self.ram_used_label = ram_used_label
-        # self.swap_used_label = swap_used_label
-        # self.disk_used_label = disk_used_label
-        self.avg_load_label = avg_load_label
-        self.temp_label = temp_label
-
-        #battery_label = Widget.Label(
-        #     label="Hello world!"
-        # )
-
-        # p = UPowerService()
-        # battery_label.set_label(', '.join([f'Battery {batt.percent}%' for batt in p.batteries]) or 'No batteries')
-
-        # Utils.Poll(1000, self.update_ratatoskr)
-        
-        box = Widget.Box(
-            spacing = 6,
-            vertical = True,
-            child = [
-                weather_box,
-                Widget.Separator(vertical=False),
-                memory_used_box,
-                # ram_used_label,
-                # Widget.Separator(vertical=False),
-                # swap_used_label,
-                Widget.Separator(vertical=False),
-                avg_load_label,
-                # temp_label,
-                # disk_used_label,
-                Widget.Separator(vertical=False),
-                network_box,
-                Widget.Separator(vertical=False),
-                # battery_label,
-                battery_box,
-                Widget.Separator(vertical=False),
-                multiline
-            ]
-        )
-
-        super().__init__(
-            namespace = 'background-infos',
-            monitor = monitor,
-            child = box,
-            layer = 'bottom',
-            anchor = ['bottom', 'left'],
-            margin_left = 70,
-            margin_bottom = 40
-        )
-        self.update_theme()
-    
-    def update_ratatoskr (self, rat):
-        if rat:
-            if 'weather' in rat: self.weather_box.update_value(rat['weather'])
-            if 'battery' in rat:
-                b = rat["battery"]
-                self.battery_box.update_value(b)
-            if 'ram' in rat: self.memory_used_box.update_value(rat['ram'])
-            if 'network'in rat: self.network_box.update_value(rat['network'])
-            if 'loadavg' in rat: self.avg_load_label.update_value(f'{rat['loadavg']['m1']} {rat['loadavg']['m5']} {rat['loadavg']['m15']}', color=gra(rat['loadavg']['warn']))
-            if 'temperature' in rat and 'disk' in rat and 'volume' in rat: self.multiline.update_value(rat['temperature'], rat['disk'], rat['volume'])
-
-            if False: battery_eta = (1, 273, .95)
-
-    def update_theme (self):
-        self.set_style(f'background-color:rgba(0,0,0,0.00001);color:{col('on_background')};')
-
 back = BackgroundInfos()
 
 ################################################################
@@ -257,7 +166,7 @@ def update_ratatoskr (_, path, event_type):
     global rat, battery_eta
     rat = read_ratatoskr_output()
 
-    if back: back.update_ratatoskr(rat)
+    # if back: back.update_ratatoskr(rat)
     # fore.update_ratatoskr(rat)
     Bus.publish(rat, topic='ratatoskr')
 
